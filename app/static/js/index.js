7a543a338bd540f328b312a7e6e8c241d519fd9d
yago <yaguitor@gmail.com>
2024-12-03 16:59:48 +0100

    Working test

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -4,12 +4,34 @@ document.addEventListener('DOMContentLoaded', async () => {
     fetch('http://127.0.0.1:5000/generate-graph')
       .then(response => response.json())
       .then(data => {
-        const main = document.getElementById('graph');
-        main.innerHTML = data.graph_html;
+        const iframe = document.getElementById('graphIframe');
+
+        const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+
+        iframeDocument.open();
+        iframeDocument.write(`
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Graph</title>
+            <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+          </head>
+          <body>
+            <main id="graph">${data.graph_html}</main>
+          </body>
+          </html>
+        `);
+        iframeDocument.close();
+
+        iframe.onload = () => {
+          const iframeHeight = iframe.contentWindow.document.documentElement.scrollHeight;
+          iframe.style.height = `${iframeHeight}px`;
+        };
       })
       .catch(error => {
         console.error("Error:", error);
       });
   });
 });
-
